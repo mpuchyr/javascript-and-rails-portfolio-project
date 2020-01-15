ae3cea7127192f79ebe0a8a733ff2a012bb07bda
mpuchyr <matt.puchyr@gmail.com>
2020-01-14 18:30:06 -0600

    Add functionality to fetchQuotes in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,10 @@ const BASE_URL = "http://localhost:3000"
 const MOVIES_URL = `${BASE_URL}/movies`
 const QUOTES_URL = `${BASE_URL}/quotes`
 
+let movies = []
+let quotes = []
+
+
 class Movie {
     constructor(name) {
         this.name = name
@@ -13,4 +17,16 @@ class Quote {
         this.quote = quote
         this.movieId = movieId
     }
+}
+
+// Quotes
+let fetchQuotes = () => {
+    return fetch(`${QUOTES_URL}`)
+    .then(res => res.json())
+    .then(quotes => {
+        quotes.forEach(quote => {
+            let newQuote = new Quote(quote.quote, quote.movie_id)
+            quotes.push(newQuote)
+        })
+    })
 }
