12946bd1150ee7041187e174835b5cfcf946d175
mpuchyr <matt.puchyr@gmail.com>
2020-01-14 18:25:27 -0600

    Create classes for Movie and Quote in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,16 @@
 const BASE_URL = "http://localhost:3000"
 const MOVIES_URL = `${BASE_URL}/movies`
 const QUOTES_URL = `${BASE_URL}/quotes`
+
+class Movie {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+class Quote {
+    constructor(quote, movieId) {
+        this.quote = quote
+        this.movieId = movieId
+    }
+}
